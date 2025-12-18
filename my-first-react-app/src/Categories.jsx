e3b8b1eca92482911d1085028b926989915fef1d
Charlize367 <charlizecmendoza@gmail.com>
2025-12-18 16:46:22 +0800

    Finalized CSS fixes

--- my-first-react-app/src/Categories.jsx
+++ my-first-react-app/src/Categories.jsx
@@ -66,10 +66,10 @@ const Categories = ()  => {
       
 
     return(
-    <div className="container">
+    <div className="w-full">
       <Nav/>
 
-        <h2>Browse {param.name} Movies</h2>
+       <h2 className="text-2xl text-white font-bold ml-10">Browse {param.name} Movies</h2>
 
       <section className="all-movies">
         {isLoading ? (
@@ -79,11 +79,17 @@ const Categories = ()  => {
         ) : errorMessage ? (
         <p>{errorMessage}</p>
       ) : (
-          <ul className="movie-display">
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2
+    md:grid-cols-3 lg:gap-8 py-6 px-4">
+  
           {movies.map((movie) => (
+             <div className="aspect-[2/3] rounded-lg overflow-hidden rounded bg-gray-300">
             <MovieCard movie={movie}/>
+            </div>
           ))}
-          </ul>
+          
+          
+          </div>
   )}
       </section>
     </div>
