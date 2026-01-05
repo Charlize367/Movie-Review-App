1a0b3dc968b8fa19cd01eb7ce0696aa8be48cac7
Charlize367 <charlizecmendoza@gmail.com>
2026-01-05 16:59:54 +0800

    Update Liked.jsx

--- my-first-react-app/src/Liked.jsx
+++ my-first-react-app/src/Liked.jsx
@@ -7,7 +7,7 @@ import {useParams} from "react-router-dom";
 import axios from "axios"
 
 const Liked = () => {
-  const API_URL = 'http://localhost:3000/api/v1';
+  const API_URL = import.meta.env.VITE_API_URL;
   const userId = localStorage.getItem('user_ID');
   const [likedMovies, setLikedMovies] = useState([]);
    const [isLoading, setIsLoading] = useState(false);
