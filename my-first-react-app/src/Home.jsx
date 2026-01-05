2c41cd19501f0152b5a7d7996809e7c111c8d757
Charlize367 <charlizecmendoza@gmail.com>
2026-01-05 17:13:26 +0800

    Update Home.jsx

--- my-first-react-app/src/Home.jsx
+++ my-first-react-app/src/Home.jsx
@@ -6,8 +6,8 @@ import MovieCard from './components/MovieCard.jsx'
 
 
 
-const apiUrl =  'https://api.themoviedb.org/3';
-const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODI1MGEyNjQ5YTAwYTk2OTdlYjIxMGUzMTExZGE1YyIsIm5iZiI6MTcyMjU4NzAzNS43MTYsInN1YiI6IjY2YWM5NzliNTEyMTNhZjA5MWJkNThhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zrM-3dtjvwa-al-qRd70tlGRD0VkxCFbHgYmZEzY6gA';
+const apiUrl =  import.meta.env.VITE_TMDB_API_URL;
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
 const apiOptions = {
   method: 'GET',
