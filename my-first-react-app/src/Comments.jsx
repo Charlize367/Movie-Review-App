dd83b4cf32f28f938f63881b2db2b992685b4936
Charlize367 <charlizecmendoza@gmail.com>
2026-01-05 16:59:20 +0800

    Update Comments.jsx

--- my-first-react-app/src/Comments.jsx
+++ my-first-react-app/src/Comments.jsx
@@ -10,7 +10,7 @@ import relativeTime from "dayjs/plugin/relativeTime"
 
 
 const Comments = () => {
-  const API_URL = 'http://localhost:3000/api/v1';
+  const API_URL = import.meta.env.VITE_API_URL;
   const apiUrl =  'https://api.themoviedb.org/3';
   const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODI1MGEyNjQ5YTAwYTk2OTdlYjIxMGUzMTExZGE1YyIsIm5iZiI6MTcyMjU4NzAzNS43MTYsInN1YiI6IjY2YWM5NzliNTEyMTNhZjA5MWJkNThhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zrM-3dtjvwa-al-qRd70tlGRD0VkxCFbHgYmZEzY6gA';
 const apiOptions = {
