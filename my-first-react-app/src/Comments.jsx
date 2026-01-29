120b501894e8f75c3673cdf1056f38f928de2008
Charlize367 <charlizecmendoza@gmail.com>
2026-01-30 06:35:49 +0800

    Fixed image display again

--- my-first-react-app/src/Comments.jsx
+++ my-first-react-app/src/Comments.jsx
@@ -211,7 +211,7 @@ const Comments = () => {
             <div class="flex items-center">
                 <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold"><img
                         class="mr-2 w-6 h-6 rounded-full"
-                        src={`${API_BASE_URL} /${c.userId.image}`}
+                        src={`${API_BASE_URL}/${c.userId.image}`}
                         alt="Michael Gough"/>{c.userId.username}</p>
                 <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
                         title="February 8th, 2022">{dayjs(c.updatedAt).fromNow()}</time></p>
